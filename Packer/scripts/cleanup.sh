16293dd2e2e7b8bd6724fc98bb1275270a37fb20
claudiusask <47273506+claudiusask@users.noreply.github.com>
2022-10-19 19:28:00 +0100

    Add files via upload

---
+++ Packer/scripts/cleanup.sh
@@ -0,0 +1,12 @@
+echo "==> Clear up yum"
+yum clean all
+
+echo "==> Clear out machine id"
+rm -f /etc/machine-id
+touch /etc/machine-id
+
+echo "==> Removing temporary files used to build box"
+rm -rf /tmp/*
+
+echo "==> resetting cloud-init"
+cloud-init clean
---
+++ Packer/scripts/yum_update.sh
@@ -0,0 +1,2 @@
+sudo yum update -y
+sudo yum install cloud-init -y
